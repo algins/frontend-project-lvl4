3662f85952e73b85c1e9d9830d661372fb65709f
algins <igors.algins@gmail.com>
2022-06-01 19:46:14 +0300

    Add react-bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import LoginPage from './LoginPage.jsx';
@@ -7,29 +11,39 @@ import NotFoundPage from './NotFoundPage.jsx';
 const App = () => (
   <Router>
     <div className="d-flex flex-column h-100">
-      <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container">
-          <a className="navbar-brand" href="/">Hexlet Chat</a>
-        </div>
-      </nav>
+      <Navbar
+        bg="white"
+        expand="lg"
+        variant="light"
+      >
+        <Container>
+          <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+        </Container>
+      </Navbar>
 
-      <div className="container-fluid h-100">
-        <div className="row justify-content-center align-content-center h-100">
-          <div className="col-12 col-md-8 col-xxl-6">
+      <Container fluid className="h-100">
+        <Row className="justify-content-center align-content-center h-100">
+          <Col
+            xs={12}
+            md={8}
+            xxl={6}
+          >
             <Switch>
               <Route exact path="/">
                 <HomePage />
               </Route>
+
               <Route path="/login">
                 <LoginPage />
               </Route>
+
               <Route path="*">
                 <NotFoundPage />
               </Route>
             </Switch>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   </Router>
 );
