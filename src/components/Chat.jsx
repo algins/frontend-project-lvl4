bcd44c41d1bee8e1d99b48b019e50978083532fa
algins <igors.algins@gmail.com>
2022-06-04 09:47:55 +0300

    Fix layout on small screens

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -45,7 +45,8 @@ const Chat = () => {
     <Container className="h-100 my-4 overflow-hidden rounded shadow">
       <Row className="h-100 bg-white flex-md-row">
         <Col
-          xs={4}
+          xs={12}
+          sm={4}
           md={2}
           className="border-end pt-5 px-0 bg-light"
         >
