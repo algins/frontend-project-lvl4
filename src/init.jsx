4d04ffe81b7c99d3cf4b8d43fa98f58d737a8f50
algins <igors.algins@gmail.com>
2022-06-12 07:28:52 +0300

    Add Rollbar

--- src/init.jsx
+++ src/init.jsx
@@ -1,3 +1,4 @@
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
@@ -11,6 +12,15 @@ import setLocale from './locales/yup/setLocale.js';
 import store from './slices/index.js';
 
 export default () => {
+  const rollbarConfig = {
+    accessToken: '08ee2c1cc9ea4e979ae63ece376be02d',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+    },
+  };
+
   const i18n = i18next.createInstance();
   i18n.init({ lng: 'ru', resources });
   setLocale(i18n);
@@ -18,14 +28,18 @@ export default () => {
   const socket = io();
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <StoreProvider store={store}>
-        <AuthProvider>
-          <SocketProvider socket={socket}>
-            <App />
-          </SocketProvider>
-        </AuthProvider>
-      </StoreProvider>
-    </I18nextProvider>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18n}>
+          <StoreProvider store={store}>
+            <AuthProvider>
+              <SocketProvider socket={socket}>
+                <App />
+              </SocketProvider>
+            </AuthProvider>
+          </StoreProvider>
+        </I18nextProvider>
+      </ErrorBoundary>
+    </Provider>
   );
 };
