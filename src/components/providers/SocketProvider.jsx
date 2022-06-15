61fdcf0dcb0cac9d72e16da763129944569af2dc
algins <igors.algins@gmail.com>
2022-06-15 07:01:03 +0300

    Move socket event handlers to init

--- src/components/providers/SocketProvider.jsx
+++ src/components/providers/SocketProvider.jsx
@@ -1,33 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import SocketContext from '../../contexts/socket.js';
-import { actions as channelsActions } from '../../slices/channelsSlice.js';
-import { actions as messagesActions } from '../../slices/messagesSlice.js';
 
-const SocketProvider = ({ socket, children }) => {
-  const dispatch = useDispatch();
-
-  socket.on('newMessage', (payload) => {
-    dispatch(messagesActions.newMessage(payload));
-  });
-
-  socket.on('newChannel', (payload) => {
-    dispatch(channelsActions.newChannel(payload));
-  });
-
-  socket.on('renameChannel', (payload) => {
-    dispatch(channelsActions.renameChannel(payload));
-  });
-
-  socket.on('removeChannel', (payload) => {
-    dispatch(channelsActions.removeChannel(payload));
-  });
-
-  return (
-    <SocketContext.Provider value={socket}>
-      {children}
-    </SocketContext.Provider>
-  );
-};
+const SocketProvider = ({ socket, children }) => (
+  <SocketContext.Provider value={socket}>
+    {children}
+  </SocketContext.Provider>
+);
 
 export default SocketProvider;
--- src/init.jsx
+++ src/init.jsx
@@ -2,14 +2,16 @@ import { Provider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { io } from 'socket.io-client';
 import { Provider as StoreProvider } from 'react-redux';
+import { io } from 'socket.io-client';
 import App from './components/App.jsx';
 import AuthProvider from './components/providers/AuthProvider.jsx';
 import SocketProvider from './components/providers/SocketProvider.jsx';
 import resources from './locales/index.js';
 import setLocale from './locales/yup/setLocale.js';
 import store from './slices/index.js';
+import { actions as channelsActions } from './slices/channelsSlice.js';
+import { actions as messagesActions } from './slices/messagesSlice.js';
 
 const init = () => {
   const rollbarConfig = {
@@ -26,6 +28,22 @@ const init = () => {
   setLocale(i18n);
 
   const socket = io();
+
+  socket.on('newMessage', (payload) => {
+    store.dispatch(messagesActions.newMessage(payload));
+  });
+
+  socket.on('newChannel', (payload) => {
+    store.dispatch(channelsActions.newChannel(payload));
+  });
+
+  socket.on('renameChannel', (payload) => {
+    store.dispatch(channelsActions.renameChannel(payload));
+  });
+
+  socket.on('removeChannel', (payload) => {
+    store.dispatch(channelsActions.removeChannel(payload));
+  });
 
   return (
     <Provider config={rollbarConfig}>
